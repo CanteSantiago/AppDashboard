d54c18ee08d2e1845c2da6bc18c306289e2069ac
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-29 16:31:22 -0500

    Vid397-Control Flow-@For

--- my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -14,6 +14,9 @@ export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
 
+  public frameworks = signal(['Angular','Vue','Svelte','Qwik','React']);
+  public frameworks2 = signal([]);
+
 
 
   public toggleContent() {
